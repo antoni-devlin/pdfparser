43b190ebea2c4bf32d7311e0840c8d59eb7a2aef
antoni-devlin <antoni.devlin@gmail.com>
2018-09-12 17:55:16 +0100

    Added unique names for output files, custom stopword support, and writetofile function, as well as opening output file on complete.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+#Author: Antoni Devlin - 2018
 import PyPDF2
 import textract
 from collections import Counter
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import subprocess
+from pathlib import Path
+from datetime import datetime
 
-filename = 'Measuring_Sustainability_WMI.pdf'
+for file in 
 
-pdfFileObj = open(filename, 'rb')
 
+pdfname = 'Measuring_Sustainability_WMI.pdf'
+pdfFileObj = open(pdfname, 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 num_pages = pdfReader.numPages
@@ -27,10 +31,10 @@ else:
 
 tokens = word_tokenize(text)
 
-punctuations = ['(',')',';',':','[',']',',','.','-']
+punctuations = ['(',')',';',':','[',']',',','.','-','&','%']
 
 stopwords = stopwords.words('english')
-custom_stopwords = ['the']
+custom_stopwords = ['the', r'{[0-9]+:[0-9]+}', r'\d+']
 stopwords.extend(custom_stopwords)
 
 keywords = [word for word in tokens if not word in stopwords and not word in punctuations]
@@ -39,12 +43,19 @@ keywords_lower = [x.lower() for x in keywords]
 
 frequency = Counter(keywords_lower)
 
-with open('pdf-output.txt' ,'w') as f:
-    rows = 0
-    for k,v in frequency.most_common():
-        f.write( "{} = {}\n".format(k,v) )
-        rows += 1
-    print('Successfully wrote ' + str(rows) + ' rows to pdf-output.txt, based on an analysis of ' + filename + '.')
 
-#Open generated file
-subprocess.call(['open', '-a', 'TextEdit', 'pdf-output.txt'])
+
+def writetofile():
+    time = datetime.now().strftime("%b %d %Y %H:%M:%S")
+    filename = 'OUTPUT_' + pdfname +str(time) + '.txt'
+    with open(filename ,'w') as f:
+        rows = 0
+        for k,v in frequency.most_common():
+            f.write( "{} = {}\n".format(k,v) )
+            rows += 1
+    print('Successfully wrote ' + str(rows) + ' rows to ' + filename + ', based on an analysis of ' + pdfname + '.')
+    #Open generated file
+    subprocess.call(['open', '-a', 'TextEdit', 'pdf-output.txt'])
+
+#Run program
+writetofile()
